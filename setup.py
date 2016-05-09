d28fa796c44db697e9595177460452b10b3ebf55
Aji John <aji.john@gmail.com>
2016-05-08 23:04:24 -0700

    upped the version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'dfaas',
     packages = ['dfaas'],
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Official API to DFAAS',
     author = 'Aji John',
     author_email = 'aji.john@gmail.com',
     url = 'https://github.com/ajijohn/dfaas',
-    download_url = 'https://github.com/ajijohn/dfaas/tarball/0.1.1',
+    download_url = 'https://github.com/ajijohn/dfaas/tarball/0.1.2',
     keywords = ['dfaas-api', 'api', 'genomic filtering'],
     classifiers = [],  
 )
